1ff47990d95ee820378558f52cd7e40999dd2fef
UtkinVadim <utkinvadim1991@gmail.com>
2023-05-01 11:12:35 +0300

    sprint 3

---
+++ src/components/ProductList.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul class="catalog__list">
+    <li class="catalog__item" v-for="(product, index) in products" :key="index">
+      <ProductItem
+        :image="product.image"
+        :title="product.title"
+        :price="product.price"
+      />
+    </li>
+  </ul>
+</template>
+
+<script>
+import ProductItem from './ProductItem.vue';
+
+export default {
+  name: 'ProductList',
+  components: { ProductItem },
+  props: ['products'],
+};
+</script>
